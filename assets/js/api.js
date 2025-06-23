1bc4701eebd6a5b91f4fef076b88b2358033a9d8
Davi Friggi <114791461+dFriggi@users.noreply.github.com>
2025-06-23 19:11:36 -0300

    Update api.js
    atualizado o link de url dentro do js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/dFriggi/js-developer-portfolio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/dFriggi/portfolio/refs/heads/main/json/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
